45fce381a868ae647f7ae9a456f023dd81adaa97
K-Danils <danils.kubiskins@gmail.com>
2021-11-27 18:41:37 +0200

    Flow of Control exercises completed

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -15,13 +15,28 @@ namespace LargestNumber
 
             Console.WriteLine("Input the 3rd number: ");
             var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+
+            if (double.Parse(input1) <= double.Parse(input2))
+            {
+                if (double.Parse(input2) <= double.Parse(input3))
+                {
+                    Console.WriteLine("Largest number is: " + input3);
+                }
+                else
+                {
+                    Console.WriteLine("Largest number is: " + input2);
+                }
+            }
+            else if (double.Parse(input1) <= double.Parse(input3))
+            {
+                Console.WriteLine("Largest number is: " + input3);
+            }
+            else
+            {
+                Console.WriteLine("Largest number is: " + input1);
+            }
+
+            Console.Read();
         }
     }
 }
--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,64 +4,60 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
             var input = Console.ReadLine();
+            long n = 0;
 
-            /*
-            todo - check if Long
-            if (?) 
+            if (long.TryParse(input, out n)) 
             {
-                //todo - check if n is less than zero
-                if (?) 
+                if (n > 0) 
                 {
                     n *= -1;
                 }
-
-                fixme
-                if (?) 
+                
+                if (n >= 10000000000) 
                 {
                     Console.WriteLine("Number is greater or equals 10,000,000,000!");
-                } 
+                }
                 else 
                 {
                     int digits = 1;
-                    if (?) 
+                    if (input.Length == 2) 
                     {
                         digits = 2;
                     } 
-                    else if (?) 
+                    else if (input.Length == 3) 
                     {
                         digits = 3;
                     } 
-                    else if (?) 
+                    else if (input.Length == 4) 
                     {
                         digits = 4;
                     } 
-                    else if (?) 
+                    else if (input.Length == 5) 
                     {
                         digits = 5;
                     } 
-                    else if (?) 
+                    else if (input.Length == 6) 
                     {
                         digits = 6;
                     } 
-                    else if (?) 
+                    else if (input.Length == 7) 
                     {
                         digits = 7;
                     } 
-                    else if (?) 
+                    else if (input.Length == 8) 
                     {
                         digits = 8;
                     } 
-                    else if (?) 
+                    else if (input.Length == 9) 
                     {
                         digits = 9;
                     } 
-                    else if (?) 
+                    else if (input.Length == 10) 
                     {
                         digits = 10;
                     }
@@ -73,7 +69,8 @@ namespace TenBillion
             {
                 Console.WriteLine("The number is not a long");
             }
-            */
+
+            Console.Read();
         }
     }
 }
