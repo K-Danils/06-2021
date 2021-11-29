020ad73ffb72c4b6d6898dd50275ce0cbe8799a8
K-Danils <danils.kubiskins@gmail.com>
2021-11-29 16:44:49 +0200

    Requested changes made

--- csharp-basics/exercises/TypesAndVariables/exercise5/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/exercise5/Program.cs
@@ -8,7 +8,7 @@ namespace exercise5
 {
     class Program
     {
-        static void writeBorder(int dashes)
+        static void WriteBorder(int dashes)
         {
             Console.Write("+");
             for (int i = 0; i < dashes; i++) { Console.Write("-"); }
@@ -22,12 +22,12 @@ namespace exercise5
             string[] musicTheoryClass = { "3", "Music Theory", "Mr. Davis" };
             string[] BCIS = { "4", "Business Computer Infomation Systems", "Mr. James" };
 
-            writeBorder(58);
+            WriteBorder(58);
             Console.WriteLine(String.Format("| {0,-2}|{1,38}|{2,15}|", englishClass[0], englishClass[1], englishClass[2]));
             Console.WriteLine(String.Format("| {0,-2}|{1,38}|{2,15}|", precalculusClass[0], precalculusClass[1], precalculusClass[2]));
             Console.WriteLine(String.Format("| {0,-2}|{1,38}|{2,15}|", musicTheoryClass[0], musicTheoryClass[1], musicTheoryClass[2]));
             Console.WriteLine(String.Format("| {0,-2}|{1,38}|{2,15}|", BCIS[0], BCIS[1], BCIS[2]));
-            writeBorder(58);
+            WriteBorder(58);
 
             Console.ReadLine();
         }
