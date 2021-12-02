2c1f11ace3dcbae91d17f09b8ef134e3488fbfe4
K-Danils <danils.kubiskins@gmail.com>
2021-12-02 12:29:52 +0200

    Loop exercises complete

---
+++ csharp-basics/exercises/Loops/Loops/NumberSquare/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSquare
+{
+    class NumberSquare
+    {
+        private int min;
+        private int max;
+
+        public NumberSquare()
+        {
+            Console.Write("Min? ");
+            this.min = int.Parse(Console.ReadLine());
+
+            Console.Write("Max? ");
+            this.max = int.Parse(Console.ReadLine());
+
+            MoveNumbers();
+        }
+
+        private void MoveNumbers()
+        { 
+            for (int i = max - 1; i >= 0; i--)
+            {
+                for (int j = max - i, count = 0; count < max; j++, count++)
+                {
+                    if (j > max) { j = 1; }
+
+                    Console.Write(j);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberSquare num = new NumberSquare();
+
+            Console.Read();
+        }
+    }
+}
---
+++ csharp-basics/exercises/Loops/Loops/Piglet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piglet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            int diceRoll, sum = 0;
+            string input = "";
+
+            Console.WriteLine("Welcome to Piglet!");
+
+            while (true)
+            {
+                diceRoll = ran.Next(0, 6) + 1;
+                sum += diceRoll;
+
+                Console.WriteLine("You rolled a " + diceRoll + "!");
+
+                if (diceRoll == 1) { Console.WriteLine("You got 0 points"); break;}
+
+                Console.Write("Roll again? ");
+
+                input = Console.ReadLine();
+                input = input.ToLower();
+
+                if (input == "no" || input == "n")
+                {
+                    Console.WriteLine("You got {0} points.", sum);
+                    Console.WriteLine();
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
