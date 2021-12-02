288a8f0257398d86d1018a19ab3189e7f0f36136
K-Danils <danils.kubiskins@gmail.com>
2021-12-02 12:00:59 +0200

    Loops exercises complete

---
+++ csharp-basics/exercises/Loops/Loops/RollTwoDice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollTwoDice
+{
+    class RollTwoDice
+    {
+        private Random ran = new Random();
+        private int dice1 = 0;
+        private int dice2 = 0;
+        private int sum = 0;
+
+        public RollTwoDice()
+        {
+            Console.Write("Desired sum: ");
+            sum = int.Parse(Console.ReadLine());
+
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            while (dice1 + dice2 != sum)
+            {
+                dice1 = ran.Next(0, 6) + 1;
+                dice2 = ran.Next(0, 6) + 1;
+                Console.WriteLine("{0} and {1} = {2}", dice1, dice2, (dice1 + dice2));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RollTwoDice dice = new RollTwoDice();
+
+            Console.Read();
+        }
+    }
+}
