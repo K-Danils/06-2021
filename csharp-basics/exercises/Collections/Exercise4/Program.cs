0d13f8b3558b2a77c21e84853d28eccd95f02f40
K-Danils <danils.kubiskins@gmail.com>
2021-12-17 15:04:39 +0200

    reverted Collections exercise 4

--- csharp-basics/exercises/Collections/Exercise4/Program.cs
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -13,17 +13,16 @@ namespace Exercise4
             int sum = 0;
             string input = Console.ReadLine();
             var numbers = input.ToCharArray().ToList();
-            
+
             while (true)
             {
-                numbers = numbers.Sum(num => (int)Math.Pow(int.Parse(num.ToString()), 2)).ToString().ToCharArray().ToList();
+                numbers.ForEach(num => sum += (int)Math.Pow(int.Parse(num.ToString()), 2));
 
-                if (int.Parse(String.Join("", numbers)) == 1) { Console.WriteLine("Number {0} is happy", input); break; }
-                else if (int.Parse(String.Join("", numbers)) == int.Parse(input)) { Console.WriteLine("Number {0} is not happy", input); break; }
+                if (sum == 1) { Console.WriteLine("Number {0} is happy", input); break; }
+                else if (sum == int.Parse(input)) { Console.WriteLine("Number {0} is not happy", input); break; }
 
-                Console.WriteLine(int.Parse(String.Join("", numbers)));
-                //numbers = sum.ToString().ToCharArray().ToList();
-                //sum = 0;
+                numbers = sum.ToString().ToCharArray().ToList();
+                sum = 0;
             }
 
             Console.ReadKey();
